c816c6cb92fd1d5a728e8926d7fcc074fb70d3b4
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-08-12 16:21:07 +0300

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Ahmed Ezz on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
